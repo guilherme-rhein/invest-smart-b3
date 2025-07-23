de4cb3b0ecfafcee4868ca71212411e655fa9d53
Guilherme Rhein <88910779+guilherme-rhein@users.noreply.github.com>
2025-07-23 09:57:06 -0300

    Update APP.py - 'Headers' change to try the connection

--- APP.py
+++ APP.py
@@ -30,7 +30,10 @@ def main():
         @st.cache_data
         def get_atv():
             url = "https://fundamentus.com.br/resultado.php"
-            headers = {"User-Agent": "Mozilla/5.0"}
+            #headers = {"User-Agent": "Mozilla/5.0"}
+            headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) "
+                       "AppleWebKit/537.36 (KHTML, like Gecko) "
+                       "Chrome/115.0.0.0 Safari/537.36"}
             response = requests.get(url, headers=headers)
             
             # Debug: TEST RESP
